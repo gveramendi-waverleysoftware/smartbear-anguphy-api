333b1f461293b2475b11853a36834474c1ef8ec0
Andre Ticona <aticona@waverleysoftware.com>
2022-01-19 17:40:21 -0400

    add allowed hosts

--- smartbear-anguphy/smartbear-anguphy/settings.py
+++ smartbear-anguphy/smartbear-anguphy/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-8#@6=!ggv+=u_o^qj@$&lxg4&rfb^*h+(0@$t+8#w))^31q6-h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
